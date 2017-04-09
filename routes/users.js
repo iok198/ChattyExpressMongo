ed4e949c4a7ae92b69cf593e294bf6e5c7f4fd27
iok198 <leadDeveloper@warrensirota.com>
2017-04-09 18:44:50 -0400

    Started on authentication logic and created files for signin, signup, and profile

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var passport = require("passport");
+
+var User = require("../models/user.model");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
