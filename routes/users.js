d925ab0b595ac564742e2e3b28df2bbd21c664a2
iok198 <leadDeveloper@warrensirota.com>
2017-04-09 18:51:48 -0400

    Handles get requests for signin/signup pages and now shows flash errors in views

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signin', function(req, res) {
+  var messages = req.flash("error");
+  res.render('signin', { errors: messages, hasErrors: messages.length > 0 });
+});
+
+router.get('/signup', function(req, res) {
+  var messages = req.flash("error");
+  res.render('signup', { errors: messages, hasErrors: messages.length > 0 });
+});
+
 module.exports = router;
