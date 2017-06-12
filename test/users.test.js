092ab4ee693dbe81b8cc63473aa5c2ce9fc856bb
iok198 <leadDeveloper@warrensirota.com>
2017-06-11 21:24:57 -0400

    Fixed problems with tests

--- test/users.test.js
+++ test/users.test.js
@@ -1,6 +1,5 @@
 import User from "../src/server/models/user.model"
 import { expect } from "chai"
-import assert from "assert"
 import mongoose from "mongoose"
 
 describe("User Tests", function () {
@@ -8,38 +7,43 @@ describe("User Tests", function () {
     mongoose.connect("localhost/ChattyDB")
   })
 
-  it("Creates user properly", function () {
+  it("Creates user properly", function (done) {
     var testUser = new User({
       username: "Test",
       password: "password"
     })
 
     testUser.save(function (err, user) {
-      expect(err).to.be(null)
-      expect(user).to.not.be(null)
+      if (err) done(err)
+
+      expect(user).to.not.be.equal(null)
+      done()
     })
   })
 
-  it("should hash password correctly", function () {
+  it("should hash password correctly", function (done) {
     User.findOne({username: "Test"}, function (err, user) {
-      expect(err).to.be(null)
+      if (err) done(err)
 
-      expect(user.password).to.not.be.empty()
+      expect(user.password).to.not.be.empty
       expect(user.password).to.not.equal("password")
+      done()
     })
   })
 
-  it("should compare passwords correctly", function () {
+  it("should compare passwords correctly", function (done) {
     User.findOne({username: "Test"}, function (err, user) {
-      expect(err).to.be(null)
+      if (err) done(err)
 
-      expect(user.validPassword(user.password)).to.be(true)
+      expect(user.validPassword("password")).to.be.true
+      done()
     })
   })
 
-  it("Deletes user properly", function () {
+  it("Deletes user properly", function (done) {
     User.remove({username: "Test"}, function (err) {
-      assert(err, null)
+      if (err) done(err)
+      else done()
     })
   })
 
