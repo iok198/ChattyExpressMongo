ec25b93d8b1b16b1d6785c417cfed0cc8b4c1409
iok198 <leadDeveloper@warrensirota.com>
2017-08-15 14:42:23 -0400

    Set up basic token verification

--- src/client/utils/requireAuth.js
+++ src/client/utils/requireAuth.js
@@ -1,5 +1,19 @@
+import verifyToken from "./verifyToken"
+
 export default function requireAuth (nextState, replace, callback) {
   const token = localStorage.getItem("token")
-  if (!token) replace('/signin')
+  if (!token) replace("/signin")
+
+  verifyToken(token)
+    .then(isAuth => {
+      console.log("Auth: " + isAuth)
+
+      if (!isAuth) {
+        console.log("Should redirect to login")
+        localStorage.removeItem("token")
+        replace("/signin")
+      }
+    })
+
   callback()
 }
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,14 +1,17 @@
 import express from "express"
 import passport from "passport"
+import jwt from "jsonwebtoken"
+
+import User from "../models/user.model"
 
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource')
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource")
 })
 
-router.post('/signin', function (req, res, next) {
+router.post("/signin", function (req, res, next) {
   passport.authenticate("signin", (err, user, token) => {
     if (err) {
       return res.json(err)
@@ -24,7 +27,7 @@ router.post('/signin', function (req, res, next) {
   })(req, res, next)
 })
 
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   passport.authenticate("signup", (err, newUser, token) => {
     if (err) {
       return res.json(err)
@@ -41,6 +44,24 @@ router.post('/signup', (req, res) => {
       })
     })
   })(req, res)
+})
+
+router.post("/verify", (req, res) => {
+  const token = req.body.token
+
+  jwt.verify(token, "secret-key", (err, decoded) => {
+    if (err) {
+      return res.json({ authorized: false })
+    }
+
+    User.findOne({username: decoded.username}, (userError, user) => {
+      if (userError) {
+        return res.json({ authorized: false })
+      }
+
+      return res.json({ authorized: true })
+    })
+  })
 })
 
 export default router
