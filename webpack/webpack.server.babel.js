3502b0343d69a2d3f2809288d03bd98a1d215a69
iok198 <leadDeveloper@warrensirota.com>
2017-08-28 23:33:56 -0400

    Fixed minor problem with .env not loading

--- webpack/webpack.server.babel.js
+++ webpack/webpack.server.babel.js
@@ -7,11 +7,11 @@ import config from "../config"
 let plugins = []
 
 if (config.nodeEnv == "production") {
-  plugins.push(new webpack.DefinePlugin({
-    "process.env": {
-      NODE_ENV: JSON.stringify("production")
-    }
-  }))
+  // plugins.push(new webpack.DefinePlugin({
+  //   "process.env": {
+  //     NODE_ENV: JSON.stringify("production")
+  //   }
+  // }))
 
   plugins.push(new webpack.optimize.UglifyJsPlugin())
 }
