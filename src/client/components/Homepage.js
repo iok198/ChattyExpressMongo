74bc797a8b0b7789488cc86762b83ff6991d4b93
Teacher <teacher@AC02SK03HGL27.local>
2017-12-05 12:04:33 -0500

    Added small changes to home screen so that it changes instructions depending if you're logged in or not

--- src/client/components/Homepage.js
+++ src/client/components/Homepage.js
@@ -1,14 +1,26 @@
 import React from "react"
+import {observer, inject} from "mobx-react"
 
+@inject("userStore")
+@observer
 class HomePage extends React.Component {
   render () {
-    return (
+    if (!this.props.userStore.loggedIn) {
+      return (
         <div>
             <h1>Chat</h1>
             <p>Hello, this is a chat app, go to login or signup to get started</p>
-
         </div>
-    )
+      ) 
+    } 
+    else {
+        return (
+          <div>
+            <h1>Chat</h1>
+            <p>Hello, to start chatting, go to the chat, or go to dashboard</p>
+          </div>
+        )
+    }
   }
 }
 
